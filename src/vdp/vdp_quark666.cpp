b23b2c251d70391e3b5eefec92d369569127cdbb
jackokring <jackokring@gmail.com>
2023-10-31 10:18:09 +0000

    extra make

---
+++ src/vdp/vdp_quark666.cpp
@@ -0,0 +1,12 @@
+#include "Arduino.h"
+
+// VDP uses various functions before their definition, so here
+// vdp-console8.h gives those functions prototypes to avoid errors
+#include "vdp_quark666.h"
+
+// VDP version-agnostic getters :)
+fabgl::SoundGenerator *getVDPSoundGenerator() {
+	return &SoundGenerator;
+}
+
+#include "vdp-quark666/video.ino"
